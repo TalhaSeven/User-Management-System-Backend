ac34c6c0a3fd9d212632cd2363ca4626ba37fb3a
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-11-02 10:02:48 +0300

    phonebook start

---
+++ src/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "1234",
+    database: "phonebook",
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+})
